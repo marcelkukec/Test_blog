72e7307a5ca9ecdd799e29f9e2f1a8ed68c296ce
marcelkukec <marcel.kukec.mk@gmail.com>
2021-11-02 13:42:19 +0100

    avtentikacija userja za editanje/deletanje posta - boljša preglednost, revizija 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
   def display_session_links
     capture do
-      concat link_to "Prijava", new_user_session_path unless user_signed_in?
-      concat " "
-      concat link_to "Registracija", new_user_registration_path unless user_signed_in?
-      concat " "
-      concat link_to "Odjava", destroy_user_session_path, method: :delete if user_signed_in?
+      if user_signed_in?
+        concat link_to "Odjava", destroy_user_session_path, method: :delete if user_signed_in?
+
+      else
+        concat link_to "Prijava", new_user_session_path unless user_signed_in?
+        concat " "
+        concat link_to "Registracija", new_user_registration_path unless user_signed_in?
+        concat " "
+      end
     end
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,18 @@
 module PostsHelper
   def display_index_post_links(post)
-    html = "<td>#{link_to 'Show', post}</td>"
-    if current_user == post.user
-      html += "<td>#{link_to 'Edit', edit_post_path(post) }</td>"
-      html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } }</td>"
+    capture do
+      concat content_tag(:td, link_to( 'Show', post))
+        if current_user == post.user
+          concat content_tag(:td, link_to( 'Edit', edit_post_path(post)))
+          concat content_tag(:td, link_to( 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
+        end
     end
-    html.html_safe
   end
   def display_show_post_links(post)
-    html = ""
     if current_user == post.user
-      html += link_to 'Edit', edit_post_path(post)
-      html += ' | '
+        link_to 'Edit', edit_post_path(post)
+        ' | '
     end
-    html += link_to 'Back', posts_path
-    html.html_safe
+    link_to 'Back', posts_path
   end
 end
