4c262ac0c35d3c80817d29886cb7b8689dc2d34d
marcelkukec <marcel.kukec.mk@gmail.com>
2022-02-24 08:51:33 +0100

    Add api

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post 'authenticate', to: 'authentication#authenticate'
-      resources :posts, only: [:index]
+      resources :posts, only: [:index, show]
     end
   end
 end
